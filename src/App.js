44c0f04ca90c4da3795021611cff7ed9b35721ac
kk5190 <kk5190krrish@gmail.com>
2018-09-11 23:16:34 +0530

    set state with weather data

--- src/App.js
+++ src/App.js
@@ -7,11 +7,29 @@ import Weather from './components/Weather';
 
 const API_KEY = "d56b7ffc8abb91891d10298bb307f0e9";
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+    
+  }
   getWeather = async (event) => {
     event.preventDefault();
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.counntry,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: '';
+    })
   }
   render() {
     return (
