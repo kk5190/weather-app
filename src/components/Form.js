b7472a9fb1312721c4097dd0982f1ddec161fe3a
kk5190 <kk5190krrish@gmail.com>
2018-09-11 22:58:55 +0530

    Add Form inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <div>
-        Form COmponet
-      </div>
+      <form>
+        <input type="text" name="city" placeholder="City..." />
+        <input type="text" name="country" placeholder="Country..." />
+        <button>Get Weather</button>
+      </form>
     );
   }
 }
