b338fc442d88765c5c039d662ba111d36ebf128a
kk5190 <kk5190krrish@gmail.com>
2018-09-11 23:31:13 +0530

    display weather data

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ class App extends Component {
     this.setState({
       temperature: data.main.temp,
       city: data.name,
-      country: data.sys.counntry,
+      country: data.sys.country,
       humidity: data.main.humidity,
       description: data.weather[0].description,
-      error: '';
+      error: ''
     })
   }
   render() {
@@ -36,7 +36,12 @@ class App extends Component {
       <div className="App">
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error} />
       </div>
     );
   }
