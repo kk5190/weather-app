49ac9b4eb2787f78795da5c4d238dd3193a8fad7
kk5190 <kk5190krrish@gmail.com>
2018-09-11 23:07:09 +0530

    add api call to open weather

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "d56b7ffc8abb91891d10298bb307f0e9";
 class App extends Component {
+  getWeather = async (event) => {
+    event.preventDefault();
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div className="App">
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
         <button>Get Weather</button>
